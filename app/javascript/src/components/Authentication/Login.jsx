91dce53ee97921814e2295824fb1fe41c34021b0
Jay Bhoyar <jaybhoyar1997@gmail.com>
2021-02-18 23:20:24 +0530

    Add login functionality

--- app/javascript/src/components/Authentication/Login.jsx
+++ app/javascript/src/components/Authentication/Login.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 
 import LoginForm from "components/Authentication/Form/LoginForm";
-// import authApi from "apis/auth";
-// import { setAuthHeaders } from "apis/axios";
-// import { setToLocalStorage } from "helpers/storage";
+import authApi from "apis/auth";
+import { setAuthHeaders } from "apis/axios";
+import { setToLocalStorage } from "helpers/storage";
 
 const Login = () => {
 	const [username, setUsername] = useState("");
@@ -12,22 +12,23 @@ const Login = () => {
 
 	const handleSubmit = async (event) => {
 		event.preventDefault();
-		// 	try {
-		// 		const response = await authApi.login({
-		// 			login: { username, password },
-		// 		});
-		// 		setToLocalStorage({
-		// 			authToken: response.data.auth_token,
-		// 			username,
-		// 			userId: response.data.userId,
-		// 		});
-		// 		setAuthHeaders();
-		// 		setLoading(false);
-		// 		window.location.href = "/";
-		// 	} catch (error) {
-		// 		console.log(error);
-		// 		setLoading(false);
-		// 	}
+		try {
+			const response = await authApi.login({
+				login: { username, password },
+			});
+			console.log(response.data);
+			setToLocalStorage({
+				authToken: response.data.auth_token,
+				username,
+				userId: response.data.userId,
+			});
+			setAuthHeaders();
+			setLoading(false);
+			window.location.href = "/";
+		} catch (error) {
+			console.log(error);
+			setLoading(false);
+		}
 	};
 
 	return (
