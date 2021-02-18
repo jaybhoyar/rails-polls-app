115060726a8f6f6743a2be737f214a4eaa6a1858
Jay Bhoyar <jaybhoyar1997@gmail.com>
2021-02-18 16:52:40 +0530

    Add user validation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: %i[create]
+
   root "home#index"
   get '*path', to: 'home#index', via: :all
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2021_02_18_105347) do
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
+    t.string "username",null: false
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
